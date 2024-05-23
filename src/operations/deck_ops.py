0f752bb5350df61d81ef0a422daedc57c0c8bbe0
Chase Kolozsy <chasekolozsy@gmail.com>
2024-05-23 11:40:12 -0600

    removes the create_filtered_deck function because this endpoint is best tested in study_ops.py

--- src/operations/deck_ops.py
+++ src/operations/deck_ops.py
@@ -2,7 +2,7 @@ import requests
 from note_ops import get_notetypes
 import json
 
-BASE_URL = "http://localhost:5000/api/decks"
+BASE_URL = "http://localhost:5001/api/decks"
 
 def create_deck(deck_name):
     response = requests.post(f"{BASE_URL}/create/{deck_name}")
@@ -109,88 +109,84 @@ if __name__ == "__main__":
 
     # Assuming the 'Basic' notetype ID is 1 (as per your instruction)
     basic_notetype_id = notetype_dict['Basic']
-    deck_id = 1
-
     # Create a deck
     create_deck_response = create_deck("New Deck")
     print(create_deck_response)
+    deck_id = create_deck_response['id']
+    print(f"deck_id: {deck_id}")
 
-    # Create a filtered deck
-    create_filtered_deck_response = create_filtered_deck("Filtered Deck", "tag:Food", 25, [1, 10])
-    print(create_filtered_deck_response)
-
-    # Change deck notetype
+    ## Change deck notetype
     change_deck_notetype_response = change_deck_notetype(deck_id, basic_notetype_id)
     print(change_deck_notetype_response)
 
-    # Set new card limit
-    set_new_card_limit_response = set_new_card_limit(deck_id, 50)
-    print(set_new_card_limit_response)
+    ## Set new card limit
+    #set_new_card_limit_response = set_new_card_limit(deck_id, 50)
+    #print(set_new_card_limit_response)
 
-    # Set current deck
-    set_current_deck_response = set_current_deck(deck_id)
-    print(set_current_deck_response)
+    ## Set current deck
+    #set_current_deck_response = set_current_deck(deck_id)
+    #print(set_current_deck_response)
 
-    # Create a configuration
-    get_deck_config_response = get_deck_config(deck_id)
-    print(get_deck_config_response)
-    deck_config = get_deck_config_response.json()['config']
+    ## Create a configuration
+    #get_deck_config_response = get_deck_config(deck_id)
+    #print(get_deck_config_response)
+    #deck_config = get_deck_config_response.json()['config']
 
-    create_config_response = create_config(
-        "Custom Config",
-        new_cards_per_day=20,
-        review_cards_per_day=100,
-        new_mix=deck_config.DeckConfig.Config.ReviewMix.REVIEW_MIX_MIX_WITH_REVIEWS,
-        interday_learning_mix=deck_config.DeckConfig.Config.ReviewMix.REVIEW_MIX_AFTER_REVIEWS,
-        review_order=deck_config.DeckConfig.Config.ReviewCardOrder.REVIEW_CARD_ORDER_DAY
-    )
-    print(create_config_response)
+    #create_config_response = create_config(
+    #    "Custom Config",
+    #    new_cards_per_day=20,
+    #    review_cards_per_day=100,
+    #    new_mix=deck_config.DeckConfig.Config.ReviewMix.REVIEW_MIX_MIX_WITH_REVIEWS,
+    #    interday_learning_mix=deck_config.DeckConfig.Config.ReviewMix.REVIEW_MIX_AFTER_REVIEWS,
+    #    review_order=deck_config.DeckConfig.Config.ReviewCardOrder.REVIEW_CARD_ORDER_DAY
+    #)
+    #print(create_config_response)
 
-    # Apply a configuration (assuming config_id is 2)
-    apply_config_response = apply_config(deck_id, 2)
-    print(apply_config_response)
+    ## Apply a configuration (assuming config_id is 2)
+    #apply_config_response = apply_config(deck_id, 2)
+    #print(apply_config_response)
 
-    # Update deck mix
-    update_deck_mix_response = update_deck_mix(
-        deck_id,
-        new_mix=deck_config.DeckConfig.Config.ReviewMix.REVIEW_MIX_BEFORE_REVIEWS,
-        interday_learning_mix=deck_config.DeckConfig.Config.ReviewMix.REVIEW_MIX_MIX_WITH_REVIEWS,
-        review_order=deck_config.DeckConfig.Config.ReviewCardOrder.REVIEW_CARD_ORDER_RANDOM
-    )
-    print(update_deck_mix_response)
+    ## Update deck mix
+    #update_deck_mix_response = update_deck_mix(
+    #    deck_id,
+    #    new_mix=deck_config.DeckConfig.Config.ReviewMix.REVIEW_MIX_BEFORE_REVIEWS,
+    #    interday_learning_mix=deck_config.DeckConfig.Config.ReviewMix.REVIEW_MIX_MIX_WITH_REVIEWS,
+    #    review_order=deck_config.DeckConfig.Config.ReviewCardOrder.REVIEW_CARD_ORDER_RANDOM
+    #)
+    #print(update_deck_mix_response)
 
-    # Delete a deck
-    delete_deck_response = delete_deck(deck_id)
-    print(delete_deck_response)
+    ## Delete a deck
+    #delete_deck_response = delete_deck(deck_id)
+    #print(delete_deck_response)
 
-    # Delete a filtered deck
-    delete_filtered_deck_response = delete_filtered_deck(deck_id)
-    print(delete_filtered_deck_response)
+    ## Delete a filtered deck
+    #delete_filtered_deck_response = delete_filtered_deck(deck_id)
+    #print(delete_filtered_deck_response)
 
-    # Rename a deck
-    rename_deck_response = rename_deck(deck_id, "Renamed Deck")
-    print(rename_deck_response)
+    ## Rename a deck
+    #rename_deck_response = rename_deck(deck_id, "Renamed Deck")
+    #print(rename_deck_response)
 
-    # Get all decks
-    get_decks_response = get_decks()
-    print(get_decks_response)
+    ## Get all decks
+    #get_decks_response = get_decks()
+    #print(get_decks_response)
 
-    # Get a specific deck
-    get_deck_response = get_deck(deck_id)
-    print(get_deck_response)
+    ## Get a specific deck
+    #get_deck_response = get_deck(deck_id)
+    #print(get_deck_response)
 
-    # Get cards in a deck
-    get_cards_in_deck_response = get_cards_in_deck(deck_id)
-    print(get_cards_in_deck_response)
+    ## Get cards in a deck
+    #get_cards_in_deck_response = get_cards_in_deck(deck_id)
+    #print(get_cards_in_deck_response)
 
-    # Get current deck ID
-    get_current_deck_id_response = get_current_deck_id()
-    print(get_current_deck_id_response)
+    ## Get current deck ID
+    #get_current_deck_id_response = get_current_deck_id()
+    #print(get_current_deck_id_response)
 
-    # Get current deck
-    get_current_deck_response = get_current_deck()
-    print(get_current_deck_response)
+    ## Get current deck
+    #get_current_deck_response = get_current_deck()
+    #print(get_current_deck_response)
 
-    # Get active decks
-    get_active_decks_response = get_active_decks()
-    print(get_active_decks_response)
+    ## Get active decks
+    #get_active_decks_response = get_active_decks()
+    #print(get_active_decks_response)
