e91f88926fd7e95404a95ab8270e8ee07785a1ca
Chase Kolozsy <chasekolozsy@gmail.com>
2024-05-28 13:04:38 -0600

    adds ability to recieve media from AnkiAPI

--- src/operations/study_ops.py
+++ src/operations/study_ops.py
@@ -1,12 +1,23 @@
 import requests
+import base64
+import os
 
 BASE_URL = 'http://localhost:5001/api'
+media_path = './data/media/tmp'
 
 def study(*, deck_id: int, action: str, username: str, base_url: str = BASE_URL) -> tuple[dict, int]:
     url = f"{base_url}/study"
     response = requests.post(url, json={"action": action, "deck_id": deck_id, "username": username})
-    
-    return response.json(), response.status_code
+    response_data = response.json()
+
+    # Process media files if present
+    if 'media_files' in response_data:
+        print(f'response_data["media_files"] = {response_data["media_files"]}')
+        for filename, filedata in response_data['media_files'].items():
+            with open(f"{media_path}/{filename}", "wb") as media_file:
+                media_file.write(base64.b64decode(filedata))
+
+    return response_data, response.status_code
 
 def create_custom_study_session(*, username: str, deck_id: int, custom_study_params: dict, base_url: str = BASE_URL) -> tuple[dict, int]:
     url = f"{base_url}/custom-study"
@@ -52,6 +63,8 @@ if __name__ == "__main__":
     import deck_ops
     import import_ops
 
+    home = Path.home()
+
     username = "testuser"
     response = user_ops.create_user(username)
     print(response)
@@ -59,37 +72,36 @@ if __name__ == "__main__":
     print(deck_id)
 
     file_name = '0_Video_Segments.apkg'
-    file_path = Path.home() / f'Documents/FromX2Ank/AnkiClient/data/apkgs/{file_name}'
+    file_path = home / f'Documents/FromX2Ank/AnkiClient/data/apkgs/{file_name}'
+    import_ops.upload_anki_package(username, file_path)
+
+    file_name = 'test.apkg'
+    file_path = home / f'Documents/FromX2Ank/AnkiClient/data/apkgs/{file_name}'
     import_ops.upload_anki_package(username, file_path)
 
     notetype = 'Basic' 
     deck_name = 'testdeck'
     delimiter = 'TAB'
 
+    file_name = 'food.zip'
+    zip_file_path = home / f'Documents/FromX2Ank/AnkiClient/data/media/{file_name}'
+    zip_file_result = import_ops.post_zip_file_to_unzip_media(zip_file_path, username)
+    print(zip_file_result)
+
     file_name = 'Food-recite.txt'
-    file_path = Path.home() / f'Documents/FromX2Ank/AnkiClient/data/csv_files/{file_name}'
+    file_path = home / f'Documents/FromX2Ank/AnkiClient/data/csv_files/{file_name}'
     import_ops.upload_csv_file(username, file_path, deck_name, notetype, delimiter)
+
+    file_name = 'directions.zip'
+    zip_file_path = home / f'Documents/FromX2Ank/AnkiClient/data/media/{file_name}'
+    zip_file_result = import_ops.post_zip_file_to_unzip_media(zip_file_path, username)
+    print(zip_file_result)
 
     file_name = 'Directions-recite.txt'
     file_path = Path.home() / f'Documents/FromX2Ank/AnkiClient/data/csv_files/{file_name}'
     import_ops.upload_csv_file(username, file_path, deck_name, notetype, delimiter)
 
     review_session(deck_id, username)
-
-
-    #custom_study_params = {
-    #    "new_limit_delta": 10,
-    #    "review_limit_delta": 20,
-    #    "forgot_days": 0,
-    #    "review_ahead_days": 5,
-    #    "preview_days": 3,
-    #    "cram": {
-    #        "kind": "CRAM_KIND_DUE",
-    #        "card_limit": 20,
-    #        "tags_to_include": ["Food"],
-    #        "tags_to_exclude": []
-    #    }
-    #}
 
     custom_study_params = {
         "new_limit_delta": 20,
@@ -119,7 +131,17 @@ if __name__ == "__main__":
 
     review_session(deck_dict['Custom Study Session'], username)
 
+
+    review_session(deck_dict['0: Video Segments'], username)
+    review_session(deck_dict['Test'], username)
+
     input = input("ready to delete user? y/n: ") 
     if input == 'y':
         print(user_ops.delete_user(username))
+        media_files = os.listdir(media_path)
+        for file in media_files:
+            file_path = os.path.join(media_path, file)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+
 
