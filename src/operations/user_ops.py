c7bd85cfde412b010e352ba555c30185ae183236
Chase Kolozsy <chasekolozsy@gmail.com>
2025-05-11 11:55:04 +0200

    misc edits to test code

--- src/operations/user_ops.py
+++ src/operations/user_ops.py
@@ -27,36 +27,44 @@ def sync_user_login(*, profile_name: str, username: str, password: str, upload: 
     except requests.exceptions.RequestException as e:
         return {"error": str(e)}
 
+#if __name__ == "__main__":
+#    from dotenv import dotenv_values
+#    from deck_ops import get_decks
+#    from import_ops import upload_anki_package
+#    from db_ops import sync_db
+#    import time
+#
+#    env_vars = dotenv_values()
+#    profile_name = "User 1"
+#
+#    # Test creating a user
+#    print(create_user(profile_name))
+#
+#    username = env_vars['ANKI_USERNAME']
+#    password = env_vars['ANKI_PASSWORD']
+#    endpoint = env_vars['ANKI_ENDPOINT']
+#
+#    #home = Path.home()
+#    #file_name = '0_Video_Segments.apkg'
+#    #file_path = Path.home() / f'Documents/FromX2Ank/AnkiClient/data/apkgs/{file_name}'
+#    #upload_anki_package(profile_name, file_path)
+#
+#    result = sync_user_login(profile_name=profile_name, username=username, password=password, endpoint=endpoint, upload=True, sync_media=True)
+#    print(result)
+#    hkey = result['hkey']
+#    print(f"hkey: {hkey}")
+#
+#
+#    # Test deleting a user
+#    input = input(f"ready to delete user '{profile_name}'? y/n: ") 
+#    if input == 'y':
+#        print(delete_user(profile_name))
+#
+#
+
 if __name__ == "__main__":
     from dotenv import dotenv_values
-    from deck_ops import get_decks
-    from import_ops import upload_anki_package
-    from db_ops import sync_db
-    import time
-
     env_vars = dotenv_values()
-    profile_name = "User 1"
-
-    # Test creating a user
-    print(create_user(profile_name))
-
-    username = env_vars['ANKI_USERNAME']
-    password = env_vars['ANKI_PASSWORD']
-    endpoint = env_vars['ANKI_ENDPOINT']
-
-    #home = Path.home()
-    #file_name = '0_Video_Segments.apkg'
-    #file_path = Path.home() / f'Documents/FromX2Ank/AnkiClient/data/apkgs/{file_name}'
-    #upload_anki_package(profile_name, file_path)
-
-    result = sync_user_login(profile_name=profile_name, username=username, password=password, endpoint=endpoint, upload=True, sync_media=True)
-    print(result)
-    hkey = result['hkey']
-    print(f"hkey: {hkey}")
-
-
-    # Test deleting a user
-    input = input(f"ready to delete user '{profile_name}'? y/n: ") 
-    if input == 'y':
-        print(delete_user(profile_name))
-
+    username = 'User 1'
+    print(delete_user(username))
+    
